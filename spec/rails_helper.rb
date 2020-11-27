1a2588135485c1b4f8e0d874b2776f2410fb8643
mjones1818 <michael.jones0532@gmail.com>
2020-11-27 13:49:12 -0600

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,16 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+  
+    # Cleaner backtrace for failure messages
+  config.backtrace_exclusion_patterns = [
+    /\/lib\d*\/ruby\//,
+    /bin\//,
+    /gems/,
+    /spec\/spec_helper\.rb/,
+    /lib\/rspec\/(core|expectations|matchers|mocks)/
+  ]
+  
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
